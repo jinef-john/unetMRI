a8b464ed008a3842b91eed2e51f8ec892f3a2e7f
Jinef <jinefjohn@gmail.com>
2025-08-25 07:52:19 +0300

    refactor: cleanup files

--- Py Code Files/lpips test.py
+++
@@ -1,6 +0,0 @@
-import sys
-sys.path.append('C:/Users/atytchino/PerceptualSimilarity/models')
-from lpips import LPIPS
-lpips_loss_fn = LPIPS(net='vgg')
-from inspect import signature
-print(signature(LPIPS.forward))
--- Py Code Files/pytorch gen chk.py
+++
@@ -1,5 +0,0 @@
-import torch
-print(torch.__version__)
-print(torch.__file__)
-print(torch.randn_like(torch.zeros(3, 3), generator=torch.Generator())
-)
